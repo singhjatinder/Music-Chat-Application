d05563b7332ab67076ca5a324bb185a140d2d4e3
Jake Diamond <jake.b.diamond@gmail.com>
2017-06-09 18:07:50 -0400

    First Commit. Added boilerplate code for project start.

---
+++ music-chat-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    YoutubeComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
