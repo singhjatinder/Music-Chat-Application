9fd0c5d30ff3778a92d5e5355b90379ec5f7250a
Jake Diamond <jake.b.diamond@gmail.com>
2017-06-25 13:48:48 -0400

    added basic routing

--- music-chat-app/src/app/app.module.ts
+++ music-chat-app/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { YoutubeComponent } from './youtube/youtube.component';
 import { ChatComponent } from './chat/chat.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     YoutubeComponent,
-    ChatComponent
+    ChatComponent,
+    SpotifyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
